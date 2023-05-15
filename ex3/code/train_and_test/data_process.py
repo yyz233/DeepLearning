7eda55ce2b1e4ebed83fcd0bd40fb1704afbc9eb
YYZ <uuz.yooomu@qq.com>
2023-05-15 16:55:33 +0800

    实验三——增加部分数据读取细节

--- ex3/code/train_and_test/data_process.py
+++ ex3/code/train_and_test/data_process.py
@@ -7,7 +7,11 @@ import csv
 import os
 
 # 全局标签名-数值的映射，由于它只会在train_data_read函数之中被初始化，所以一定要先执行train_data_read函数
-label_map = defaultdict(int)
+name2label_map = defaultdict(int)
+# 全局标签数值-名的映射，由于它只会在train_data_read函数之中被初始化，所以一定要先执行train_data_read函数
+label2name_map = defaultdict(str)
+# 当前图片序号-图片文件名的映射，用于结果产出
+num2name_map = defaultdict(str)
 
 
 class MyDataset(Dataset):
@@ -23,7 +27,7 @@ class MyDataset(Dataset):
     def __getitem__(self, item):
         return self.transform(self.data[item][:]), self.label[item]
 
-A
+
 def train_data_read(size):
     """
     :param size: 将图像resize为size*size
@@ -35,7 +39,8 @@ def train_data_read(size):
     labels = []
     # 读取数据
     for i, image_dir_path in enumerate(image_dir_paths):
-        label_map[image_dir_path] = i
+        name2label_map[image_dir_path] = i
+        label2name_map[str(i)] = image_dir_path
         image_names = next(os.walk(path + '\\' + image_dir_path))[2]
         for image_name in image_names:
             image = cv2.imread(path + '\\' + image_dir_path + '\\' + image_name)
@@ -43,8 +48,6 @@ def train_data_read(size):
             image = cv2.resize(image, (size, size), interpolation=cv2.INTER_AREA)
             data.append(image)
             labels.append(i)
-    for i in label_map:
-        print(i, label_map[i])
 
     return data, labels
 
@@ -58,6 +61,7 @@ def test_data_read(size):
     data = []
     labels = []
     image_label_map = defaultdict(str)
+    test_number = 0
     with open('.\\data\\sample_submission.csv') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=',')
         next(spamreader)
@@ -65,44 +69,34 @@ def test_data_read(size):
             image_label_map[row[0]] = row[1]
 
     images_names = next(os.walk(path))[2]
-    for image_name in images_names:
+    for i, image_name in enumerate(images_names):
+        test_number += 1
+        num2name_map[str(i)] = image_name
         image = cv2.imread(path + '\\' + image_name)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         image = cv2.resize(image, (size, size), interpolation=cv2.INTER_AREA)
         data.append(image)
-        labels.append(label_map[image_label_map[image_name]])
-    return data, labels
+        labels.append(name2label_map[image_label_map[image_name]])
+    return data, labels, test_number
 
 
-def data_process(batch_size, size):
+def data_process(batch_size, size, transform):
     """
     如果要读取数据，只需要调用这个函数即可
     :param batch_size: 含义就像他的名字一样
     :param size: 图像resize之后的大小为size*size
+    :param transform: 图像变换
     :return: 两个torch.utils.data.DataLoader对象的实例，分别意味着train_data_loader 和test_data_loader
     注：transform部分我不太明白什么样的比较好，于是我采用了之前实验所使用的学长的transform ————yyz
     """
-    my_transform = transforms.Compose(
-        [transforms.ToPILImage(),
-         # transforms.Resize((224, 224)),
-         transforms.ToTensor(),
-         transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                              std=[0.229, 0.224, 0.225])]
-    )
+    my_transform = transform
     train_data, train_labels = train_data_read(size)
-    test_data, test_labels = test_data_read(size)
+    test_data, test_labels, test_number = test_data_read(size)
     train_dataset = MyDataset(train_data, train_labels, my_transform)
     test_dataset = MyDataset(test_data, test_labels, my_transform)
-    return torch.utils.data.DataLoader(
+    return label2name_map, torch.utils.data.DataLoader(
         train_dataset,
         batch_size=batch_size,
         num_workers=8,
         shuffle=True
-    ), torch.utils.data.DataLoader(
-        test_dataset,
-        batch_size=batch_size,
-        num_workers=8,
-        shuffle=True
-    )
-
-
+    ), test_dataset, test_number, num2name_map
